4071bd9b5aac9e066a41af42f7978219faee040b
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:51:54 -0500

    Adds test program improvements.

--- test/test.cpp
+++ test/test.cpp
@@ -8,15 +8,23 @@
 
 static constexpr std::size_t SampleRate = 44'100;
 static constexpr double Amp = 0.2;
+static constexpr std::array<double, 3> Ratios = {1., 1.2, 1.5}; // Minor triad
 
 int main()
 {
-    std::vector<float> samples(100); // = 441 Hz
-    for(std::size_t i = 0; i < samples.size(); ++i)
+    std::vector<paz::AudioTrack> tracks;
+    for(auto n : Ratios)
     {
-        samples[i] = Amp*std::sin(i*2.*M_PI/(samples.size() - 1));
+        std::vector<float> samples(100/n);
+        for(std::size_t i = 0; i < samples.size(); ++i)
+        {
+            samples[i] = Amp/n*std::sin(i*2.*M_PI/(samples.size() - 1));
+        }
+        tracks.emplace_back(samples);
     }
-    paz::AudioTrack track(samples);
-    paz::AudioEngine::Play(track, true);
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    for(const auto& n : tracks)
+    {
+        paz::AudioEngine::Play(n, true);
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
 }
