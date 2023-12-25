87363de984303fe01d5318de603b884b21a55a04
Ben Reifler <ben.reifler@gmail.com>
2023-12-25 13:56:34 -0600

    Minor update (new naming convention for non-`const` `static` variables).

--- audio_engine.cpp
+++ audio_engine.cpp
@@ -17,22 +17,23 @@
 #include <stdexcept>
 
 #ifdef PAZ_MACOS
-static AudioComponent OutputComp;
-static AudioComponentInstance OutputInstance;
+static AudioComponent _outputComp;
+static AudioComponentInstance _outputInstance;
 #else
-static PaStream* Stream;
+static PaStream* _stream;
 #endif
 
 static constexpr double SampleRate = 44'100.;
 static constexpr unsigned long FramesPerBuf = 1024;
-static std::mutex Mx;
-static std::condition_variable Cv;
-static bool BufComplete;
-static std::array<std::uint8_t, 2> MasterVol;
-static std::array<double, 2> MasterFreqScale = {1, 1};
+
+static std::mutex _mx;
+static std::condition_variable _cv;
+static bool _bufComplete;
+static std::array<std::uint8_t, 2> _masterVol;
+static std::array<double, 2> _masterFreqScale = {1, 1};
 // [samplesPtr, bufStartIndices, loop]
 static std::vector<std::tuple<std::uintptr_t, std::array<std::size_t, 2>, bool>>
-    ActiveTracks;
+    _activeTracks;
 
 #ifdef PAZ_MACOS
 static OSStatus audio_callback_stereo(void*, AudioUnitRenderActionFlags*, const
@@ -48,27 +49,27 @@ static int audio_callback_stereo(const void*, void* outBuf, unsigned long
 #endif
     static std::array<std::uint8_t, 2> vol;
     std::fill(out, out + 2*framesPerBuffer, 0);
-    std::lock_guard<std::mutex> lk(Mx);
+    std::lock_guard<std::mutex> lk(_mx);
     for(unsigned long i = 0; i < framesPerBuffer; ++i)
     {
-        for(const auto& n : ActiveTracks)
+        for(const auto& n : _activeTracks)
         {
             const auto* samples = reinterpret_cast<const std::vector<float>*>(
                 std::get<0>(n));
             for(int k = 0; k < 2; ++k)
             {
                 const std::size_t idx = std::round(std::get<1>(n)[k] + i*
-                    MasterFreqScale[k]);
+                    _masterFreqScale[k]);
                 out[2*i + k] += (*samples)[idx%samples->size()];
             }
         }
         for(int j = 0; j < 2; ++j)
         {
-            if(vol[j] < MasterVol[j])
+            if(vol[j] < _masterVol[j])
             {
                 ++vol[j];
             }
-            else if(vol[j] > MasterVol[j])
+            else if(vol[j] > _masterVol[j])
             {
                 --vol[j];
             }
@@ -76,19 +77,19 @@ static int audio_callback_stereo(const void*, void* outBuf, unsigned long
             out[2*i + j] = std::max(-1.f, std::min(1.f, out[2*i + j]*v*v));
         }
     }
-    for(auto& n : ActiveTracks)
+    for(auto& n : _activeTracks)
     {
         for(int k = 0; k < 2; ++k)
         {
             std::get<1>(n)[k] = static_cast<std::size_t>(std::round(std::get<1>(
-                n)[k] + framesPerBuffer*MasterFreqScale[k]))%reinterpret_cast<
+                n)[k] + framesPerBuffer*_masterFreqScale[k]))%reinterpret_cast<
                 const std::vector<float>*>(std::get<0>(n))->size();
         }
     }
-    if(!BufComplete)
+    if(!_bufComplete)
     {
-        BufComplete = true;
-        Cv.notify_all();
+        _bufComplete = true;
+        _cv.notify_all();
     }
 #ifdef PAZ_MACOS
     return noErr;
@@ -118,13 +119,13 @@ paz::AudioInitializer::AudioInitializer()
     desc.componentFlagsMask = 0;
     desc.componentManufacturer = kAudioUnitManufacturer_Apple;
 
-    OutputComp = AudioComponentFindNext(nullptr, &desc);
-    if(!OutputComp || AudioComponentInstanceNew(OutputComp, &OutputInstance))
+    _outputComp = AudioComponentFindNext(nullptr, &desc);
+    if(!_outputComp || AudioComponentInstanceNew(_outputComp, &_outputInstance))
     {
         throw std::runtime_error("Failed to open default audio device.");
     }
 
-    if(AudioUnitInitialize(OutputInstance))
+    if(AudioUnitInitialize(_outputInstance))
     {
         throw std::runtime_error("Failed to initialize audio unit instance.");
     }
@@ -138,7 +139,7 @@ paz::AudioInitializer::AudioInitializer()
     streamFormat.mBitsPerChannel = 32;
     streamFormat.mBytesPerPacket = 2*sizeof(float);
     streamFormat.mBytesPerFrame = 2*sizeof(float);
-    if(AudioUnitSetProperty(OutputInstance, kAudioUnitProperty_StreamFormat,
+    if(AudioUnitSetProperty(_outputInstance, kAudioUnitProperty_StreamFormat,
         kAudioUnitScope_Input, 0, &streamFormat, sizeof(streamFormat)))
     {
         throw std::runtime_error("Failed to set audio unit input property.");
@@ -146,7 +147,7 @@ paz::AudioInitializer::AudioInitializer()
 
     AURenderCallbackStruct callback;
     callback.inputProc = audio_callback_stereo;
-    if(AudioUnitSetProperty(OutputInstance,
+    if(AudioUnitSetProperty(_outputInstance,
         kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Input, 0,
         &callback, sizeof(AURenderCallbackStruct)))
     {
@@ -154,7 +155,7 @@ paz::AudioInitializer::AudioInitializer()
             "dio unit.");
     }
 
-    if(AudioOutputUnitStart(OutputInstance))
+    if(AudioOutputUnitStart(_outputInstance))
     {
         throw std::runtime_error("Failed to start audio unit.");
     }
@@ -184,7 +185,7 @@ paz::AudioInitializer::AudioInitializer()
         throw std::runtime_error("No audio devices found.");
     }
 
-    error = Pa_OpenDefaultStream(&Stream, 0, 2, paFloat32, SampleRate,
+    error = Pa_OpenDefaultStream(&_stream, 0, 2, paFloat32, SampleRate,
         FramesPerBuf, audio_callback_stereo, nullptr);
     if(error != paNoError)
     {
@@ -192,7 +193,7 @@ paz::AudioInitializer::AudioInitializer()
             Pa_GetErrorText(error)));
     }
 
-    error = Pa_StartStream(Stream);
+    error = Pa_StartStream(_stream);
     if(error != paNoError)
     {
         throw std::runtime_error("Failed to start audio stream: " + std::string(
@@ -202,19 +203,19 @@ paz::AudioInitializer::AudioInitializer()
 
     // Wait for first buffer to be completed.
     {
-        std::unique_lock<std::mutex> lk(Mx);
-        Cv.wait(lk, [](){ return BufComplete; });
+        std::unique_lock<std::mutex> lk(_mx);
+        _cv.wait(lk, [](){ return _bufComplete; });
     }
 }
 
 paz::AudioInitializer::~AudioInitializer()
 {
 #ifdef PAZ_MACOS
-    AudioOutputUnitStop(OutputInstance);
-    AudioComponentInstanceDispose(OutputInstance);
+    AudioOutputUnitStop(_outputInstance);
+    AudioComponentInstanceDispose(_outputInstance);
 #else
-    Pa_StopStream(Stream);
-    Pa_CloseStream(Stream);
+    Pa_StopStream(_stream);
+    Pa_CloseStream(_stream);
     Pa_Terminate();
 #endif
 }
@@ -227,8 +228,8 @@ void paz::AudioEngine::Play(const paz::AudioTrack& track, bool loop)
 
     std::thread([track, loop]()
     {
-        std::lock_guard<std::mutex> lk(Mx);
-        ActiveTracks.emplace_back(reinterpret_cast<std::uintptr_t>(track.
+        std::lock_guard<std::mutex> lk(_mx);
+        _activeTracks.emplace_back(reinterpret_cast<std::uintptr_t>(track.
             _samples.get()), std::array<std::size_t, 2>{}, loop);
     }
     ).detach();
@@ -239,14 +240,14 @@ void paz::AudioEngine::SetVolume(double vol, int ear)
     vol = std::max(0., std::min(1., vol));
     if(ear < 0)
     {
-        std::lock_guard<std::mutex> lk(Mx);
-        MasterVol[0] = std::round(vol*255.);
-        MasterVol[1] = std::round(vol*255.);
+        std::lock_guard<std::mutex> lk(_mx);
+        _masterVol[0] = std::round(vol*255.);
+        _masterVol[1] = std::round(vol*255.);
     }
     else if(ear == 0 || ear == 1)
     {
-        std::lock_guard<std::mutex> lk(Mx);
-        MasterVol[ear] = std::round(vol*255.);
+        std::lock_guard<std::mutex> lk(_mx);
+        _masterVol[ear] = std::round(vol*255.);
     }
 }
 
@@ -254,13 +255,13 @@ void paz::AudioEngine::SetFreqScale(double scale, int ear)
 {
     if(ear < 0)
     {
-        std::lock_guard<std::mutex> lk(Mx);
-        MasterFreqScale[0] = scale;
-        MasterFreqScale[1] = scale;
+        std::lock_guard<std::mutex> lk(_mx);
+        _masterFreqScale[0] = scale;
+        _masterFreqScale[1] = scale;
     }
     else if(ear == 0 || ear == 1)
     {
-        std::lock_guard<std::mutex> lk(Mx);
-        MasterFreqScale[ear] = scale;
+        std::lock_guard<std::mutex> lk(_mx);
+        _masterFreqScale[ear] = scale;
     }
 }
